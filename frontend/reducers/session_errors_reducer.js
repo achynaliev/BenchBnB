ad8682e36dd7a4a1cbeafee186598801c5ae3fd4
Atai Chynaliev <chynaliev.ae@gmail.com>
2018-04-02 16:47:03 -0700

    authentication is working

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -5,14 +5,13 @@ const _nullState = {
   session: []
 };
 
-export const sessionReducer = (state = [], action) => {
+export default (state = [], action) => {
   Object.freeze(state);
   let newState;
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
       return [];
     case RECEIVE_ERRORS:
-    console.log(action.errors);
       return action.errors
     default:
       return state;
